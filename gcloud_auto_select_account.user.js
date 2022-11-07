89deb4c493fcab9a8157915bab3243b3129c2f0c
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2022-11-07 16:34:14 +0100

    attempt to clean up structure

--- gcloud_auto_select_account.user.js
+++ gcloud_auto_select_account.user.js
@@ -5,8 +5,9 @@
 // @author       Vegar Sechmann Molvig
 // @match        https://accounts.google.com/o/oauth2/auth*
 // @match        https://accounts.google.com/signin/oauth/consent*
+// @match        https://accounts.google.com/signin/v2/challenge/pwd*
 // ==/UserScript==
-(function() {
+(function () {
     'use strict';
     const initialDelay = 500;
     const retryInterval = 100;
@@ -24,52 +25,86 @@
         }
     }
 
-    function getEmailFromUrl(){
-        //https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
-        const params = new Proxy(new URLSearchParams(window.location.search), {
-            get: (searchParams, prop) => searchParams.get(prop),
-        })
 
-        return params.autoAccountSelect
-    }
+    function selectAccount() {
+        const getEmailFromUrl = () => {
+            //https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+            const params = new Proxy(new URLSearchParams(window.location.search), {
+                get: (searchParams, prop) => searchParams.get(prop),
+            })
 
-    function selectAccount(email) {
-        return () => {
-            console.log("Checking for account selector")
+            return params.autoAccountSelect
+        }
+
+        const email = getEmailFromUrl()
+        if (!email) {
+            console.log("select: no 'autoAccountSelect' query parameter, stopping script")
+            return
+        }
+
+        const fn = () => {
+            console.log("select: checking for account selector")
             let accountSelector = document.querySelector(`[data-identifier="${email}"]`);
-            if (accountSelector != null) {
-                console.log("Selecting NAV account (if possible)")
-                accountSelector.click()
-            } else {
-                console.log("no account selec found yet")
+            if (!accountSelector) {
+                console.log("select: no account select found (yet)")
+                return setTimeout(fn, retryInterval)
             }
 
-            setTimeout(selectAccount, retryInterval)
+            accountSelector.click()
         }
+
+        return fn
     }
 
     function approve() {
-        console.log("Checking for approve access button")
-        let submitButton = document.querySelector('[id="submit_approve_access"]');
-        if (submitButton != null) {
-            console.log("Approving access")
-            return submitButton.click();
-        } else {
-            setTimeout(approve, retryInterval)
+        const fn = () => {
+            console.log("approve: checking for approve access button")
+            let submitButton = document.querySelector('[id="submit_approve_access"]');
+            if (!submitButton) {
+                console.log("approve: no approve button (yet, retrying)")
+                return setTimeout(fn, retryInterval)
+            }
+
+            submitButton.click()
         }
+
+        return fn
     }
 
-    if (window.location.href.startsWith("https://accounts.google.com/o/oauth2/auth")) {
-        const email = getEmailFromUrl()
-        if (email) {
-            console.log("auto selecting:", email)
-            docReady(selectAccount(email))
-        } else {
-            console.log("no query param 'autoAccountSelect', ignoring")
+
+    function login() {
+        const fn = () => {
+            console.log("login: checking for next button")
+            const nextButton = document.querySelector('[id="passwordNext"]')
+            if (!nextButton) {
+                console.log("login: no next button (yet, retrying)")
+                return setTimeout(fn, retryInterval)
+            }
+
+            console.log("login: checking for password input")
+            const passwordInput = document.querySelector('[name="password"]')
+            if (!passwordInput) {
+                console.log("login: no password input (yet, retrying)")
+                return setTimeout(fn, retryInterval)
+            }
+            console.log("login: checking for password input value")
+            if (!passwordInput.value.length > 0) {
+                console.log("login: no password value (yet, retrying)")
+                return setTimeout(fn, retryInterval)
+            }
+
+            nextButton.click()
         }
-    } else {
-        console.log("auto approving")
-        docReady(approve)
+        return fn
     }
 
+    [
+        { pathStartsWith: "/o/oauth2/auth", handler: selectAccount },
+        { pathStartsWith: "/signin/oauth/consent", handler: approve },
+        { pathStartsWith: "/signin/v2/challenge/pwd", handler: login },
+    ].forEach((e) => {
+        if (window.location.href.startsWith("https://accounts.google.com" + e.pathStartsWith)) {
+            docReady(e.handler())
+        }
+    })
 })();
